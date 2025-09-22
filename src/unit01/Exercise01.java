542efd6d1dca52e1e4f86561471013e32d1402e2
jaime.martin <jaime.martin@106-15.info2>
2025-09-22 09:33:54 +0200

    Exercise 1 - Pide un número al usuario y luego lo muestre.

---
+++ src/unit01/Exercise01.java
@@ -0,0 +1,22 @@
+package unit01;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+/* Diseña un programa que pida un número al usuario
+ * y a continuación lo muestre.
+ */
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		// Declaro variables
+		int numero;
+		// Pido un número al usuario
+		System.out.println("Introduzca un número entero");
+		// Leo de la consola
+		numero = reader.nextInt();
+		// Imprimo el numero
+		System.out.println("El número introducido es: "+numero);
+		
+	}
+
+}
