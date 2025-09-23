013a12b0793c895cacf710f1c3680fab3607fe65
jaime.martin <jaime.martin@106-15.info2>
2025-09-23 12:31:34 +0200

    subida pasos exercise05 (analisis)

---
+++ src/unit01/PasosExercise05.java
@@ -0,0 +1,17 @@
+package unit01;
+
+public class PasosExercise05 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1. Pedir el radio al usuario - Mostrar mensaje y esperar a que el usuario lo introduzca
+		 * 2. El usuario introduce el radio
+		 * 3. Calculo la longitud
+		 * 4. Imprimo la longitud
+		 * 5. Calculo el área
+		 * 6. Imprimo el área
+		 */
+
+	}
+
+}
