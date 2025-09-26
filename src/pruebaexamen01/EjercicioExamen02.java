7a7742aa56d9a19510ba912c98707419f9f10ab1
jaime.martin <jaime.martin@106-15.info2>
2025-09-26 13:45:35 +0200

    Actualizado

--- src/pruebaexamen01/EjercicioExamen02.java
+++ src/pruebaexamen01/EjercicioExamen02.java
@@ -7,8 +7,19 @@ public class EjercicioExamen02 {
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 		int horasTrabajadas;
-		
-		
+		double precioPorHora;
+		double impuestos;
+		double salarioSemanalBruto;
+		double salarioSemanalNeto;
+		System.out.println("Introduce nº de horas trabajadas");
+		horasTrabajadas = reader.nextInt();
+		System.out.println("Introduce cuantos cobras por hora");
+		precioPorHora = reader.nextDouble();
+		salarioSemanalBruto = horasTrabajadas * precioPorHora;
+		impuestos = salarioSemanalBruto < 1000 ? salarioSemanalBruto*0.1 : salarioSemanalBruto*0.15;
+
+		salarioSemanalNeto = salarioSemanalBruto - impuestos;
+		System.out.println("Tu salario semanal neto es "+salarioSemanalNeto);
 		reader.close();
 
 	}
